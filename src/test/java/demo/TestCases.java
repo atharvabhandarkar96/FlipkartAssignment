bb45844ea060fbce7e376b4cf0fd0d438430adb8
Atharva Bhandarkar <atharvabhandarkar@Atharvas-MacBook-Air.local>
2024-11-15 23:04:54 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,15 +1,22 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -51,13 +58,93 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+       
     }
 
+    @Test
+    public void testCase01() throws InterruptedException
+    {
+        System.out.println("Test Case 1 - Look for high rated Washing Machines");
+        driver.get("https://www.flipkart.com/");
+        Thread.sleep(4000);
+        Assert.assertTrue(driver.getCurrentUrl().contains("flipkart"));
+
+    
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+       
+        WebElement searchBox=wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@title='Search for Products, Brands and More']")));
+         
+        Wrappers.searchMethod(searchBox,"Washing Machine");
+
+        List<WebElement> sortingType=driver.findElements(By.xpath(".//div[@class='zg-M3Z']"));
+        Wrappers.sort(sortingType,"Popularity");
+
+        List<WebElement> listOfRatings=driver.findElements(By.xpath(".//div[@class='XQDdHH']"));
+        Wrappers.countRatings(listOfRatings);
+
+
+       
+    }
+
+    @Test
+    public void testCase02() throws InterruptedException
+    {
+        System.out.println("Test Case 2 - Look for phones with certain discount");
+        driver.get("https://www.flipkart.com/");
+        Assert.assertTrue(driver.getCurrentUrl().contains("flipkart"));
+    
+        Thread.sleep(4000);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+       WebElement searchBox=wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@title='Search for Products, Brands and More']"))); 
+       Wrappers.searchMethod(searchBox,"iPhone");
+
+        
+
+        List<WebElement> dicsounts=driver.findElements(By.xpath(".//div[@class='UkUFwK']//span"));
+        Wrappers.countDiscounts(dicsounts);
+
+
+       
+    }
+
+    @Test
+    public void testCase03() throws InterruptedException
+    {
+        System.out.println("Test Case 3 - Look for coffee mugs with certain discount");
+        
+        driver.get("https://www.flipkart.com/");
+        Thread.sleep(4000);
+        Assert.assertTrue(driver.getCurrentUrl().contains("flipkart"));
+    
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+       
+       WebElement searchBox=wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@title='Search for Products, Brands and More']")));
+        Wrappers.searchMethod(searchBox,"Coffee Mug");
+
+        List<WebElement> ratingCheckbox=driver.findElements(By.xpath(".//div[@class='_6i1qKy']"));
+        Wrappers.sort(ratingCheckbox,"4★ & above");
+        List<WebElement> sortingType=driver.findElements(By.xpath(".//div[@class='zg-M3Z']"));
+        Wrappers.sort(sortingType,"Popularity");
+
+        List<WebElement> titles=driver.findElements(By.xpath(".//a[@class='wjcEIp']"));
+        List<WebElement> imageURL=driver.findElements(By.xpath(".//a[@class='VJA3rP']"));
+        
+        Wrappers.printItems(titles);
+        Wrappers.printItems2(imageURL);
+
+        
+
+        
+
+       
+    }
     @AfterTest
     public void endTest()
     {
+       
         driver.close();
-        driver.quit();
+        
+       driver.quit();
 
     }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,5 +1,6 @@
 package demo.wrappers;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -19,4 +20,89 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    public static void searchMethod(WebElement element, String text) throws InterruptedException
+    {
+        element.click();
+        Thread.sleep(1000);
+        element.sendKeys(text);
+        element.submit();
+
+
+    }
+
+    public static void sort(List<WebElement> element, String text)throws InterruptedException
+    
+    {
+        for(WebElement e: element)
+        {
+            if(e.getText().contains(text))
+            {
+                e.click();
+                Thread.sleep(1000);
+            }
+        }
+       
+    }
+
+    public static void countRatings(List<WebElement> element)throws InterruptedException
+    
+    {
+        int count=0;
+        for(WebElement e: element)
+        {
+            String rating=e.getText();
+            double num=Double.parseDouble(rating.trim());
+            
+            if(num==4 || num<4)
+            {
+                count=count+1;
+            }
+        }
+
+        System.out.println("Total producs of rating less or equal to a are: "+count);
+       
+    }
+
+
+    public static void countDiscounts(List<WebElement> element)throws InterruptedException
+    
+    {
+        int count=0;
+        for(WebElement e: element)
+        {
+            String longDiscount=e.getText();
+            String discount=longDiscount.substring(0,2);
+            int num=Integer.parseInt(discount.trim());
+            
+            if(num>17)
+            {
+                count=count+1;
+            }
+        }
+
+        System.out.println("Total products of dicsount more than 17% are  "+count);
+       
+    }
+
+    public static void printItems(List<WebElement> element)throws InterruptedException
+    
+    {
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(element.get(i).getText());
+        }
+    }
+
+    public static void printItems2(List<WebElement> element)throws InterruptedException
+    
+    {
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(element.get(i).getAttribute("href"));
+        }
+    }
+
+    
+    
 }
+
